d870256b21bba6eaade870fe6f6247a2fba53508
Jon Roes <jon@skookum.com>
2013-10-10 14:47:18 -0400

    Signing up should redirect you to the configured path.

--- goth_test.go
+++ goth_test.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"strings"
 	"testing"
 )
@@ -29,13 +30,14 @@ func TestSignUpGetRendersWithPathPrefix(t *testing.T) {
 	}
 }
 
-func TestSignupPostHandled(t *testing.T) {
+func TestSignupPostRedirects(t *testing.T) {
 	ts, _ := setup()
-	resp, err := http.Get(ts.URL + "/auth/sign_up")
+	resp, err := http.PostForm(ts.URL+"/auth/sign_up",
+		url.Values{"email": {"jon@example.com"}, "password": {"password"}})
 	if err != nil {
-		t.Errorf("Error visiting sign up page: %v", err)
+		t.Errorf("Error posting to sign up route: %v", err)
 	}
-	if resp.StatusCode > 400 {
-		t.Errorf("Error status when visiting sign up page: %d", resp.StatusCode)
+	if resp.StatusCode != 301 {
+		t.Errorf("Expected 301 redirect after posting to sign up route. Got %d instead.", resp.StatusCode)
 	}
 }
